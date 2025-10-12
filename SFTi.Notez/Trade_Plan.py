5d0a5bb576c63f7a1935cd9ec7e8b29ccd843fa6
statikfintechllc <ascend.gremlin@gmail.com>
2025-10-12 03:17:29 +0000

    Adjusting

---
+++ SFTi.Notez/Trade_Plan.py
@@ -0,0 +1,29 @@
+<div align="center">
+
+**Trading Plan**  
+  🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫
+ 🚫APPLE,NVDA,TSLA,MSFT,GOOG🚫
+  🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫
+
+<img width="2048" height="1679" alt="image" src="" />
+
+</div>
+
+**THE TARGET**:  
+  - The Dip n Rip(1-2 tight red ticks)
+  - VWAP HOLD/BOUNCE
+  - Stocks coiling about to Rip.
+
+<div align="center">
+    
+**Indicator_Strategy_1**:  
+VWAP, MACD, EMA 25-SMA 70 & 200-BOLL
+
+**Indicator_Strategy_2**:  
+EMA 25-SMA 70 & 200-BOLL-MACD-SO
+
+**Indicator_Strategy_3**:  
+EMA/SMA 70-BOLL-MACD-SO
+
+  ✅Stay Ahead of the Fuckin Market✅ 
+—————————------—————————
